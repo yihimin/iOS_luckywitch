60caabe544fc28aa5ca07dc65fef6b1e8b9914be
yihimin <yihimin01@gmail.com>
2024-11-28 15:27:39 +0900

    Initial Commit

---
+++ nameLoveTests/nameLoveTests.swift
@@ -0,0 +1,17 @@
+//
+//  nameLoveTests.swift
+//  nameLoveTests
+//
+//  Created by Dahyun on 11/28/24.
+//
+
+import Testing
+@testable import nameLove
+
+struct nameLoveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
